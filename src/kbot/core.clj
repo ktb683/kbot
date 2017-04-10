feabb7128fef6901cdef3ebc999cb39c55b49098
Boris Kourtoukov <boriskourt@gmail.com>
2017-04-10 12:52:53 -0400

    Quick HTTP get example.

--- src/kbot/core.clj
+++ src/kbot/core.clj
@@ -1,7 +1,19 @@
 (ns kbot.core
+  (:require
+   [manifold.deferred :as d]
+   [aleph.http :as http]
+   [byte-streams :as bs])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  ;; Basic get request.
+  (-> @(http/get "https://google.com/"
+                 {;:oauth-token ""
+                  :throw-exceptions false
+                  :as :json})
+      :body
+      bs/to-string
+      prn))
