3f90a099b06fec4b158af5a84c96e110613b6df7
Boris Kourtoukov <boriskourt@gmail.com>
2017-04-10 12:51:42 -0400

    Add aleph and ancient.

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+                 [aleph "0.4.4-alpha2"]]
+  :plugins [[lein-ancient "0.6.10"]]
   :main ^:skip-aot kbot.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
