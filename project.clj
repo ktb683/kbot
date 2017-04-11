905dbf1bd47ad1fdb15f3211273f88c87516bcd9
Katie <katie.ballinger@gmail.com>
2017-04-11 19:38:53 -0400

    add function to filter monitors with @kbot and extract code block from message

--- project.clj
+++ project.clj
@@ -6,6 +6,8 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [cprop "0.1.10"]
                  [cheshire "5.7.0"]
+                 [endophile "0.2.1"]
+                 [hiccup "1.0.5"]
                  [aleph "0.4.4-alpha2"]]
   :plugins [[lein-ancient "0.6.10"]]
   :main ^:skip-aot kbot.core
--- src/kbot/core.clj
+++ src/kbot/core.clj
@@ -1,11 +1,15 @@
 (ns kbot.core
   (:require
-   [manifold.deferred :as d]
-   [aleph.http :as http]
-   [cheshire.core :as json]
-   [cprop.core :refer [load-config]]
-   [byte-streams :as bs]
-   [clojure.pprint :refer [pprint]])
+    [manifold.deferred :as d]
+    [aleph.http :as http]
+    [cheshire.core :as json]
+    [clojure.string :as s]
+    [endophile.core :refer [mp to-clj html-string]]
+    [endophile.hiccup :refer [to-hiccup]]
+    [hiccup.core :refer [html]]
+    [cprop.core :refer [load-config]]
+    [byte-streams :as bs]
+    [clojure.pprint :refer [pprint]])
   (:gen-class))
 
 (def config (load-config))
@@ -22,13 +26,25 @@
                       "&application_key="
                       (:app-key config))
                  {:as :json})
-      :body
-      ;;bs/to-string
-      ;;(json/parse-string true)
-      ))
-;;"Linux \"main fleet\" available container ratio is low"
-;;"231670"
+      :body))
+
+(defn make-hats []
+  (spit "hats.edn" (into [] (api-get "monitor" {}))))
+
+(defn find-kbot [m]
+  (filter (fn [mon] (and (= "Alert" (:overall_state mon)) (s/includes? (:message mon) "@kbot"))) m))
+
+(defn find-code-block [monitor]
+  (->>
+    (to-hiccup (mp (:message monitor)))
+    (filterv (fn [x] (= :code (first (second x)))))
+    flatten
+    (filterv string?)
+    (assoc monitor :command)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  #_(spit "hats.edn" (into [] (api-get "monitor" {}))))
+  (->> monitors
+       find-kbot
+       (map find-code-block)))
